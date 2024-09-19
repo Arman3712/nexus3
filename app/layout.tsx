9a131f132e0b9f95f5deb920657376e177e56de2
Art_of_coding <115411268+Arman3712@users.noreply.github.com>
2024-09-19 19:56:09 +0530

    Update layout.tsx metas

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta property="og:image" content="Link preview image URL">
+          <meta property="twitter:image" content="Twitter link preview image URL">
+          <meta property="twitter:card" content="summary_large_image">
+          <meta property="twitter:title" content="Twitter link preview title">
+          <meta property="twitter:description" content="Twitter link preview description">
+          <meta property="og:site_name" content="Link preview site name">
+          <meta property="og:title" content="Link preview title">
+          <meta property="og:url" content="Canonical link preview URL">
+            <meta property="og:description" content="Link preview description" />
         <script
         key="structured-1"
         type="application/ld+json"
