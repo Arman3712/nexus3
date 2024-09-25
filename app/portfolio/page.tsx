bccd5d6f36ae1a0b102a4b17b17b0d7bd73d6538
Art_of_coding <115411268+Arman3712@users.noreply.github.com>
2024-09-25 12:02:21 +0530

    Update page.tsx

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,6 +4,7 @@
 import React from "react";
 import { HeroParallax } from "@/components/ui/hero-parallax";
 import Navbar from "@/components/navbar";
+import { NextSeo } from 'next-seo';
 
 export const products = [
   {
@@ -56,6 +57,17 @@ export default function HeroParallaxDemo() {
 
   return (
     <div className="w-full md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <NextSeo
+        title="our work ! - Nexus IT "
+        description="Nexus ITS is a full fledge modern and best performant websites development agency, a agency specializing in full-stack development, responsive design, and SEO."
+        canonical="https://nexusit.in/portfolio"
+        openGraph={{
+          url: 'https://nexusit.in/portfolio',
+          title: 'our work ! - Arman kureshi',
+          description: 'Nexus IT is a leading provider of graphic design and web development services, dedicated to transforming your digital presence. Our talented team of designers and developers work collaboratively to create visually stunning and highly functional websites. We specialize in delivering custom web solutions and captivating graphic designs that align with your brand identity. From designing logos and marketing materials to building responsive websites and web applications, we ensure every project is executed with precision and creativity. Partner with us to enhance your brand’s online presence and achieve your business goals',
+          images: [{ url: 'https://github.com/Arman3712/nexus3/blob/main/public/images/Frame%2023.png?raw=true' }],
+        }}
+      />
       <Navbar
         scrollToWebsiteDesign={() => {}}
         scrollToGraphicDesign={() => {}}
