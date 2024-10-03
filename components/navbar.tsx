55bb2396bd90dc0b4e255e88b9197139db6db580
Art_of_coding <115411268+Arman3712@users.noreply.github.com>
2024-10-04 02:10:15 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -37,7 +37,7 @@ const Navbar = ({
           <Link className="cursor-pointer" href="/">
             <Image
               priority
-              src="/nexus.svg"
+              src="/heronexus.png"
               alt="Logo"
               width={100}
               height={70}
