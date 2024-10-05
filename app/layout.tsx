e7744ce7d68a607be27e9e6d3f5e4b00598da3d4
Art_of_coding <115411268+Arman3712@users.noreply.github.com>
2024-10-06 02:12:23 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,7 +84,7 @@ export default function RootLayout({
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(faqData) }}
         />
-         <Script
+         <script
          async
          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1708221248229738"
          crossOrigin="anonymous"
